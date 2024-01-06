be385c6b66b6ead7e41c77c96b3fb6b1a24f4ce7
prins mishra <mishraprins@outlook.com>
2024-01-06 20:56:19 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+ import React from 'react'
+ import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Home from  './pages/Home'
+import Header from './components/Header'
+import PrivateRoute from './components/PrivateRouter'
+  import CreateListing from './pages/CreateListing.jsx'
+import UpdateListing from './pages/UpdateListing.jsx'
+import Listing from './pages/Listing.jsx'
+import Search from './pages/Search.jsx'
+ function App() {
+   return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/search' element={<Search />} />
+      <Route path='/listing/:listingId' element={<Listing />} />
+      <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
+          </Route>
+     
+      
+
+      
+     
+
+  
+
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
+      
+      
+   )
+ }
+ 
+ export default App
---
+++ client/src/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUg9oGknViOZ8wijAq3kjoFc23uB6S2Uk",
+  authDomain: "mern-estate-567a6.firebaseapp.com",
+  projectId: "mern-estate-567a6",
+  storageBucket: "mern-estate-567a6.appspot.com",
+  messagingSenderId: "151285470760",
+  appId: "1:151285470760:web:12ad3b6513a941eb04c71d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
